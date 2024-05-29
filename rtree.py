911f672f283e4b6ae72c11ae4cdb853a8a6de4bf
Hasti <hastortarik4@gmail.com>
2024-05-29 22:39:51 +0200

    endgame

---
+++ rtree.py
@@ -0,0 +1,9 @@
+
+"""
+svaki ulaz u cvor je oblika (mbr, oid) gdje je oid id tacke
+Rstablo reda(m, M) ima:
+1. Svaki cvor ima najmanje m slogova i najvise M
+2. Svi listovi su na istom nivou
+3. Cvorovi sadrze (mbr, p) gdje p pokazuje na dijete
+"""
+class rtree:
